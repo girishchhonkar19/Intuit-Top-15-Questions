0a1a7eee137d720e81aabf2f0b910dbef272c6fd
Girish Kumar <85754888+girishchhonkar19@users.noreply.github.com>
2022-01-25 12:31:03 +0530

    Time: 92 ms (44.27%), Space: 18.8 MB (58.65%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -11,13 +11,18 @@ public:
                     q.push({i,j});
             }
          }
-        
+        if(q.size()==0 || q.size()==n*n) return -1;
         int ans=0;
         int dx[4]={1,-1,0,0};
         int dy[4]={0,0,1,-1};
+        int step=0;
         while(!q.empty())
         {
-            int x=q.front().first;
+            int qsize=q.size();
+            step++;
+            for(int j=0;j<qsize;j++)
+            {
+                int x=q.front().first;
             int y=q.front().second;
             q.pop();
             for(int i=0;i<4;i++)
@@ -25,17 +30,20 @@ public:
                 int nx=x+dx[i];
                 int ny=y+dy[i];
                 
-                if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0)
-                {
+                if(nx<0 || nx>=n || ny<0 || ny>=n || grid[nx][ny]==1)
+                    continue;
+                
                     q.push({nx,ny});
                     
-                    grid[nx][ny]=grid[x][y]+1;
+                    grid[nx][ny]=1;
                     
-                    ans=max(ans,grid[nx][ny]);
-                }
+                    ans=max(ans,step);
+                
+            }
+            
             }
             
         }
-        return ans-1;
+        return ans;
     }
 };
